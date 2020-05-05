26b9aa44ddc85e7cce1b8731f57c1bb46034848b
scottbrumley <scott@nsisecure.com>
2020-05-05 11:03:49 -0400

    Fixed auto build

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
+## Make sure Demisto script is executable
 chmod +x /vagrant/demistoserver-5.5-67560.sh
 
-#sudo /vagrant/demistoserver-5.5-67560.sh --quiet
+sudo /vagrant/demistoserver-5.5-67560.sh -- -y
 
-echo Login in using 'vagrant ssh' command then install Demisto by /vagrant/scripts/install.sh
+#echo Login in using 'vagrant ssh' command then install Demisto by /vagrant/scripts/install.sh
